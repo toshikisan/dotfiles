dbec34ea6481825b1b23c432baeaf282239b420a
ctoshiki <toshikisys@gmail.com>
2019-10-17 22:47:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My dotfiles with ansible
+
+Set up a local development environment with ansible.
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function apt () {
+  if !(type "git" > /dev/null 2>&1); then
+    sudo apt install -y git
+  fi
+
+  if !(type "ansible-playbook" > /dev/null 2>&1); then
+    sudo apt install -y ansible
+  fi
+}
+
+function deploy () {
+  hosts='./ansible/inventory/hosts'
+  playbook='./ansible/playbook.yml'
+  ansible-playbook -i $hosts $playbook
+}
+
+apt
+deploy
