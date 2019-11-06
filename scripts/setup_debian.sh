4da168d37b561888915d8b1863a070ac533e8537
ctoshiki <toshikisys@gmail.com>
2019-11-07 08:47:24 +0900

    modify scripts/setup_debian.sh

---
+++ scripts/setup_debian.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if !(type "git" > /dev/null 2>&1); then
+  sudo apt install -y git
+fi
+
+if !(type "ansible-playbook" > /dev/null 2>&1); then
+  sudo apt install -y ansible
+fi
+
+
+# git clone dotfiles
+DOTPATH=$HOME/dotfiles
+GIT_REPO=https://github.com/ctoshiki/dotfiles.git
+
+if [ ! -d "$DOTPATH" ]; then
+  git clone "$GIT_REPO" "$DOTPATH"
+else
+  echo "$DOTPATH already downloaded. Updating..."
+  cd "$DOTPATH"
+  git stash
+  git checkout master
+  git pull origin master
+  echo
+fi
+
+HOSTS='$HOME/dotfiles/ansible/inventory/hosts'
+PLAYBOOK='$HOME/dotfiles/ansible/playbook.yml'
+ansible-playbook -i $HOSTS $PLAYBOOK --ask-vault-pass
