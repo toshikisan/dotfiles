0eb671b389e71da772b72dc01ce33db8924c2da0
ctoshiki <toshikisys@gmail.com>
2019-11-07 09:12:13 +0900

    modify scripts/setup_debian.sh

--- scripts/setup_debian.sh
+++ scripts/setup_debian.sh
@@ -24,6 +24,6 @@ else
   echo
 fi
 
-HOSTS='$HOME/dotfiles/ansible/inventory/hosts'
-PLAYBOOK='$HOME/dotfiles/ansible/debian.yml'
+HOSTS=$HOME/dotfiles/ansible/inventory/hosts
+PLAYBOOK=$HOME/dotfiles/ansible/debian.yml
 ansible-playbook -i $HOSTS $PLAYBOOK --ask-vault-pass
